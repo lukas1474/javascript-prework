16ea3b880b5cb196d172d0e617c690e5d4f6a4fa
Lukasz Sobel <lukas1474@gmail.com>
2020-04-07 23:54:37 +0200

    add buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+function playGame(playerInput) {
+
 function getMoveName(argMoveId){
   if(argMoveId == 1){
     return 'kamień';
@@ -42,7 +44,7 @@ let argComputerMove = getMoveName(randomNumber);
 
 printMessage('Mój ruch to: ' + argComputerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+//let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
@@ -51,3 +53,18 @@ let argPlayerMove = getMoveName(playerInput);
 displayResult(argComputerMove, argPlayerMove);
 
 printMessage(`Twój ruch to: ${argPlayerMove}`);
+}
+
+clearMessages ();
+
+document.getElementById('play-rock').addEventListener('click', function(argPlayerMove = 'kamień'){
+  playGame('1');
+});
+
+document.getElementById('play-paper').addEventListener('click', function(argPlayerMove = 'papier'){
+  playGame('2');
+});
+
+document.getElementById('play-scissors').addEventListener('click', function(argPlayerMove = 'nożyce'){
+  playGame('3');
+});
